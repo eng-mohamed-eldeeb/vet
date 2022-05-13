4c3ba1e16c013ba08346b4925f323b22d3de5122
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-05-13 23:23:38 +0200

    Add data and query

--- data.sql
+++ data.sql
@@ -10,3 +10,18 @@ INSERT INTO animals VALUES (5, 'Angemon' ,'2005-6-12', 1, true,-45);
 INSERT INTO animals VALUES (5, 'Boarmon' ,'2005-6-7', 7, true,20.4);
 INSERT INTO animals VALUES (5, 'Blossom' ,'1998-10-13', 3, true,17);
 INSERT INTO animals VALUES (5, 'Ditto' ,'2022-5-14', 4, true,-22);
+
+INSERT INTO owners (full_name, age) VALUES 
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38)
+;
+
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS null;
