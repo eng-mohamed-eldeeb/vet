fba8dd7a53b89f4d16f4b405e264ffb1a99f64e6
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-05-13 18:20:40 +0200

    Create owners and species tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,32 @@ CREATE TABLE animals (
 
 ALTER table animals
 add species varchar(50);
+
+CREATE TABLE owners (
+id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(100),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals
+  ADD COLUMN species_id INT,
+  ADD CONSTRAINT fk_species_id
+    FOREIGN KEY (species_id)
+    REFERENCES species(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE animals
+  ADD COLUMN owner_id INT,
+  ADD CONSTRAINT fk_owner_id
+    FOREIGN KEY (owner_id)
+    REFERENCES owners(id)
+    ON DELETE CASCADE;
